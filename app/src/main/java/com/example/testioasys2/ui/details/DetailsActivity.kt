f636c9c3a49959cbf69466cbf6d0d94ac9d29088
jorgemiguel7 <jorgemiguel.jm18@gmail.com>
2022-05-30 23:58:37 -0300

    implementando Compose na tela de DetailsActivity.kt

--- app/src/main/java/com/example/testioasys2/ui/details/DetailsActivity.kt
+++ app/src/main/java/com/example/testioasys2/ui/details/DetailsActivity.kt
@@ -3,30 +3,72 @@ package com.example.testioasys2.ui.details
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.engine.DiskCacheStrategy
-import com.example.testioasys2.databinding.ActivityDetailsBinding
-import com.example.testioasys2.ui.base.BaseActivity
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.example.testioasys2.R
 import com.example.testioasys2.utils.Constants
+import com.google.android.material.composethemeadapter.MdcTheme
 
-class DetailsActivity : BaseActivity() {
-    private lateinit var binding: ActivityDetailsBinding
-
+class DetailsActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityDetailsBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        setupToolbar(binding.detailsToolbar, intent.getStringExtra(EXTRA_NAME)!!, true)
-        setDetails()
+        setContent {
+            MdcTheme {
+                Scaffold(
+                    topBar = {
+                        MyTopAppBar(
+                            enterpriseName = intent.getStringExtra(EXTRA_NAME).orEmpty(),
+                            onClickNavigationIcon = { finish() }
+                        )
+                    },
+                    backgroundColor = Color.White
+                ) {
+                    Box(modifier = Modifier.padding(it)) {
+                        AddScrollView(
+                            enterprisePhoto = intent.getStringExtra(EXTRA_PHOTO).orEmpty(),
+                            enterpriseDescription = intent.getStringExtra(EXTRA_DESCRIPTION).orEmpty(),
+                        )
+                    }
+                }
+            }
+        }
     }
 
-    companion object{
-        private const val EXTRA_NAME = "EXTRA_NAME"
-        private const val EXTRA_PHOTO = "EXTRA_PHOTO"
-        private const val EXTRA_DESCRIPTION = "EXTRA_DESCRIPTION"
+    companion object {
+        const val EXTRA_NAME = "EXTRA_NAME"
+        const val EXTRA_PHOTO = "EXTRA_PHOTO"
+        const val EXTRA_DESCRIPTION = "EXTRA_DESCRIPTION"
 
-        fun getStartIntent(context: Context, name: String, photo: String, description: String): Intent{
+        fun getStartIntent(context: Context, name: String, photo: String, description: String): Intent {
             return Intent(context, DetailsActivity::class.java).apply {
                 putExtra(EXTRA_NAME, name)
                 putExtra(EXTRA_PHOTO, photo)
@@ -34,11 +76,78 @@ class DetailsActivity : BaseActivity() {
             }
         }
     }
+}
 
-    private fun setDetails() {
-        Glide.with(this@DetailsActivity).load(Constants.PHOTO_BASE_URL
-            .plus(intent.getStringExtra(EXTRA_PHOTO)))
-            .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC).into(binding.detailsPhotoImageView)
-        binding.detailsDescriptionTextView.text = intent.getStringExtra(EXTRA_DESCRIPTION)
+@Composable
+fun AddScrollView(enterprisePhoto: String = "", enterpriseDescription: String = "") {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize(1F)
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        item {
+            AsyncImage(
+                model = Constants.PHOTO_BASE_URL
+                    .plus(enterprisePhoto),
+                contentDescription = stringResource(id = R.string.enterprise_photo_content_description),
+                modifier = Modifier
+                    .width(318.dp)
+                    .height(155.dp)
+            )
+            Text(
+                text = enterpriseDescription,
+                fontFamily = FontFamily(
+                    Font(R.font.source_sans_pro_regular),
+                ),
+                fontSize = 18.sp,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 20.dp, start = 16.dp, end = 16.dp),
+                color = colorResource(id = R.color.grey),
+                textAlign = TextAlign.Justify,
+            )
+        }
+    }
+}
+
+@Composable
+fun MyTopAppBar(enterpriseName: String = "", onClickNavigationIcon: () -> Unit = {}) {
+    TopAppBar(title = { Text(text = enterpriseName) },
+        contentColor = colorResource(id = android.R.color.white),
+        backgroundColor = Color.Transparent,
+        modifier = Modifier.background(
+            Brush.verticalGradient(
+                colors = listOf(
+                    colorResource(id = R.color.medium_pink),
+                    colorResource(id = R.color.black_pink)
+                )
+            )
+        ),
+        navigationIcon = {
+            IconButton(onClick = onClickNavigationIcon) {
+                Icon(
+                    Icons.Default.ArrowBack,
+                    stringResource(id = R.string.action_back_content_description)
+                )
+            }
+        })
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MdcTheme {
+        Scaffold(
+            topBar = {
+                MyTopAppBar("Enterprise Name")
+            },
+            backgroundColor = Color.White,
+        ) {
+            Box(modifier = Modifier.padding(it)) {
+                AddScrollView(enterpriseDescription = "Description")
+            }
+        }
     }
 }
